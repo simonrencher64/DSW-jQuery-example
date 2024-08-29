7f4b06244a26f7f097790d250b289f8722d215b3
simonrencher64 <simonrencher64@gmail.com>
2024-08-29 14:18:06 -0700

    update

--- myscript.js
+++ myscript.js
@@ -3,7 +3,25 @@ $(document).ready(function() {
 		$("img").toggle();
 	});
 	$("h1").click(function() {
+		$("p").removeClass("red");
 		$("p").addClass("bright");
 		$("img").addClass("border");
+	});
+	$("h2").click(function() {
+		$("p").removeClass("bright");
+		$("img").removeClass("border");
+		$("p").addClass("red");
+	});
+	$("#p1").click(function() {
+		$(".disappear").toggle();
+	});
+	$("#freakytoggler").click(function() {
+		$("#freaky").toggle();
+	});
+	$("img").mouseenter(function() {
+		$("img").addClass("coolBorder");
+	});
+	$("img").mouseleave(function() {
+		$("img").removeClass("coolBorder");
 	});
 });
